08ebe6c7a20a5e9ef76f942426cb72165cdfc3cc
norraist <norraist@gmail.com>
2024-11-29 17:05:43 -0500

    change name in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="DA-Map-Zoom",
+    name="foundry_map_tool",
     version="0.1.0",
     description="A tool to adjust the scale of maps in JSON files for Foundry VTT.",
     author="Rory Burke",
